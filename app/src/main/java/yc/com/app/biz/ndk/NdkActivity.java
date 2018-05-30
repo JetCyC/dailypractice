d47d7d425f6e10e0f6621a9a12865ca379262ab4
caoyanchang <ex-caoyanchang@xianglin.cn>
2018-05-30 09:27:34 +0800

    jni  ndk

---
+++ app/src/main/java/yc/com/app/biz/ndk/NdkActivity.java
@@ -0,0 +1,18 @@
+package yc.com.app.biz.ndk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yc.com.app.R;
+
+public class NdkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ndk);
+    }
+
+
+
+}
